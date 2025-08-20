f722f172afac518cd0cff8f0564148233f022ef8
ARTHUR DE ANDRADE SANTANA <162452096+caiua2669@users.noreply.github.com>
2025-08-20 11:45:55 +0000

    [fix] correção retorno jason

--- api/cadastro.js
+++ api/cadastro.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { nome, email } = req.body;
-    res.status(200).json({ mensagem: `Cadastro recebido: ${nome} (${email})` });
+   
     alert(`${nome}, seja bem vindo!`)
   } else {
     res.status(405).json({ mensagem: "Método não permitido" });
